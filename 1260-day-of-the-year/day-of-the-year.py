a0e6957c76bd1d4b2ed467c9b9af2877496d3215
sanyamtyagi229 <sanyamtyagitauli@gmail.com>
2026-09-17 17:42:30 +0530

    Time: 11 ms (53.74%) | Memory: 19.3 MB (63.20%) - LeetSync

---
+++ 1260-day-of-the-year/day-of-the-year.py
@@ -0,0 +1,11 @@
+class Solution:
+    def dayOfYear(self, date: str) -> int:
+        year = int(date[0:4])
+        month = int(date[5:7])
+        day = int(date[8:10])
+        days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+            days_in_month[1] = 29  
+        total_days = sum(days_in_month[:month-1]) + day
+        
+        return total_days
